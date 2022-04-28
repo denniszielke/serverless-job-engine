1110b3bc565a76a57bff6a219a9320912674a635
denniszielke <11569044+denniszielke@users.noreply.github.com>
2022-04-28 10:59:34 +0200

    added host

--- src/Engine/src/main/java/org/acme/PingResource.java
+++ src/Engine/src/main/java/org/acme/PingResource.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import java.net.InetAddress;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,5 +13,21 @@ public class PingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Pong!";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String host() {
+        String localHostName = null;
+
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            localHostName = address.getHostName();
+
+        }catch (Exception e) {
+            
+        }
+        
+        return localHostName;
     }
 }
