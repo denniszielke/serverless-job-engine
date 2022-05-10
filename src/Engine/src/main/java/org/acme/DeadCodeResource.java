432012b50a598d1e3cf565ae29218d8f4c9eaba0
denniszielke <11569044+denniszielke@users.noreply.github.com>
2022-05-10 18:48:12 +0200

    added deadcode

---
+++ src/Engine/src/main/java/org/acme/DeadCodeResource.java
@@ -0,0 +1,47 @@
+package org.acme;
+
+import java.net.InetAddress;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import io.dapr.client.DaprClient;
+import io.dapr.client.domain.StateOptions;
+import io.dapr.client.domain.CloudEvent;
+import io.dapr.client.domain.State;
+
+@Path("/deadcode")
+public class DeadCodeResource {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Inject
+    DaprClient daprClient;
+
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response receive(byte[] body) {
+        
+        CloudEvent event = null;
+        CloudEvent event2 = null;
+        try{
+            event = new CloudEvent<>();
+            event2 = new CloudEvent<>("id", "source", "type", "specversion", body);
+            
+        }catch (Exception e) {
+            logger.error("Something went wrong during dapr interaction while processing counter state.");
+            logger.error(e.getMessage(), e);
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
+        return Response.ok(event).build();
+    }
+}
